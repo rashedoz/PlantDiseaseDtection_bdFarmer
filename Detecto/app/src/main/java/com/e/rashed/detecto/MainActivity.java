6968d3af1707420686476c702f853da1e287c35c
Rashed Mazumder <yaan.uchiha29@gmail.com>
2019-02-07 22:14:41 +0600

    Android upload

--- Detecto/app/src/main/java/com/e/rashed/detecto/MainActivity.java
+++ Detecto/app/src/main/java/com/e/rashed/detecto/MainActivity.java
@@ -3,14 +3,28 @@ package com.e.rashed.detecto;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     ImageView mImageView;
+    private StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button cameraBtn = (Button) findViewById(R.id.cameraBtn);
         mImageView = (ImageView) findViewById(R.id.imageView);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("h1");
+
+        myRef.setValue("Hello, World!");
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +61,39 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+            Bitmap bm = Bitmap.createScaledBitmap(imageBitmap, 256, 256, true);
+            mImageView.setImageBitmap(bm);
+
+            //Saving file in internal
+            //Preparing for upload
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data2 = baos.toByteArray();
+
+            // Create a storage reference from our app
+            //creating reference to firebase storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReferenceFromUrl("gs://diseasedetect-e39f6.appspot.com");    //change the url according to your firebase app
+
+            // Create a reference to "mountains.jpg"
+            StorageReference mountainsRef = storageRef.child("mountains.jpg");
+
+            UploadTask uploadTask = mountainsRef.putBytes(data2);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.e("Debug","Upload failed");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    Log.e("Debug","Upload Success");
+                }
+            });
+
         }
     }
 
